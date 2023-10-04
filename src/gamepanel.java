c0625ba8cc0657fb600db9565f3c1bad9bb7acfc
Amolgithu <129654129+Amolgithu@users.noreply.github.com>
2023-10-04 17:45:07 +0530

    updates

--- src/gamepanel.java
+++ src/gamepanel.java
@@ -7,56 +7,66 @@ import javax.swing.JPanel;
 
 public class gamepanel extends JPanel {
 
-    private Color allcolor[][] = new Color[15][10],owncolor;
+    private Color allcolor[][] = new Color[15][10], owncolor;
     // private Color sblock[][] = { { null, Color.red, Color.red, null },
-    //         { Color.red, Color.red, null, null },
-    //         { null, null, null, null } };
+    // { Color.red, Color.red, null, null },
+    // { null, null, null, null } };
 
     Random r = new Random();
-    private int[] differencex = new int[4],difference = new int[4];
-    private int[][][]
-                    allshapesx={
-                                {{4,5,4,5},{4,5,4,5},{4,5,4,5},{4,5,4,5}},
-                                {{3,4,5,6},{4,4,4,4},{3,4,5,6},{4,4,4,4}},
-                                {{4,5,3,4},{4,4,5,5},{4,5,3,4},{4,4,5,5}},
-                                {{3,4,4,5},{4,3,4,3},{3,4,4,5},{4,3,4,3}},
-                                {{5,3,4,5},{4,4,4,5},{3,4,5,3},{4,5,5,5}},
-                                {{3,3,4,5},{4,5,4,4},{3,4,5,5},{5,5,4,5}},
-                                {{4,3,4,5},{4,4,5,4},{3,4,5,4},{5,4,5,5}}
-                    },
-                    allshapesy={
-                                {{0,0,1,1},{0,0,1,1},{0,0,1,1},{0,0,1,1}},
-                                {{0,0,0,0},{0,1,2,3},{0,0,0,0},{0,1,2,3}},
-                                {{0,0,1,1},{0,1,1,2},{0,0,1,1},{0,1,1,2}},
-                                {{0,0,1,1},{0,1,1,2},{0,0,1,1},{0,1,1,2}},
-                                {{0,1,1,1},{0,1,2,2},{0,0,0,1},{0,0,1,2}},
-                                {{0,1,1,1},{0,1,2,2},{0,0,0,1},{0,1,2,2}},
-                                {{0,1,1,1},{0,1,1,2},{0,0,0,1},{0,1,1,2}}
-                    };
+    private int[] differencex = new int[4], difference = new int[4];
+    private int[][][] allshapesx = {
+            { { 4, 5, 4, 5 }, { 4, 5, 4, 5 }, { 4, 5, 4, 5 }, { 4, 5, 4, 5 } },
+            { { 3, 4, 5, 6 }, { 4, 4, 4, 4 }, { 3, 4, 5, 6 }, { 4, 4, 4, 4 } },
+            { { 4, 5, 3, 4 }, { 4, 4, 5, 5 }, { 4, 5, 3, 4 }, { 4, 4, 5, 5 } },
+            { { 3, 4, 4, 5 }, { 4, 3, 4, 3 }, { 3, 4, 4, 5 }, { 4, 3, 4, 3 } },
+            { { 5, 3, 4, 5 }, { 4, 4, 4, 5 }, { 3, 4, 5, 3 }, { 4, 5, 5, 5 } },
+            { { 3, 3, 4, 5 }, { 4, 5, 4, 4 }, { 3, 4, 5, 5 }, { 5, 5, 4, 5 } },
+            { { 4, 3, 4, 5 }, { 4, 4, 5, 4 }, { 3, 4, 5, 4 }, { 5, 4, 5, 5 } }
+    },
+            allshapesy = {
+                    { { 0, 0, 1, 1 }, { 0, 0, 1, 1 }, { 0, 0, 1, 1 }, { 0, 0, 1, 1 } },
+                    { { 0, 0, 0, 0 }, { 0, 1, 2, 3 }, { 0, 0, 0, 0 }, { 0, 1, 2, 3 } },
+                    { { 0, 0, 1, 1 }, { 0, 1, 1, 2 }, { 0, 0, 1, 1 }, { 0, 1, 1, 2 } },
+                    { { 0, 0, 1, 1 }, { 0, 1, 1, 2 }, { 0, 0, 1, 1 }, { 0, 1, 1, 2 } },
+                    { { 0, 1, 1, 1 }, { 0, 1, 2, 2 }, { 0, 0, 0, 1 }, { 0, 0, 1, 2 } },
+                    { { 0, 1, 1, 1 }, { 0, 1, 2, 2 }, { 0, 0, 0, 1 }, { 0, 1, 2, 2 } },
+                    { { 0, 1, 1, 1 }, { 0, 1, 1, 2 }, { 0, 0, 0, 1 }, { 0, 1, 1, 2 } }
+            };
 
-    private int sblockx[] = { 4, 5, 3, 4 }, sblocky[] = { 0, 0, 1, 1 };
-    private boolean move = true,lose;
-    private int nowshape,nextshape;
+    private int sblockx[] = new int[4], sblocky[] = new int[4];
+    // private int sblockx[] = { 4, 5, 3, 4 }, sblocky[] = { 0, 0, 1, 1 };
+    private boolean move = true, lose;
+    private int nowshape, nextshape;
     public int rotation = 0;
 
     // ArrayList<Integer> sblocky = new ArrayList<Integer>(2);
     // ArrayList<Integer> sblockx = new ArrayList<Integer>(4);
 
     public gamepanel() {
-        owncolor = new Color(r.nextInt(0,255),r.nextInt(0,255),r.nextInt(0,255));
+        nowshape = r.nextInt(0, 7);
+        owncolor = new Color(r.nextInt(0, 255), r.nextInt(0, 255), r.nextInt(0, 255));
         initialpanelcolor();
+        setshape();
         setcolorsarray();
         setBounds(0, 0, 400, 600);
         setBackground(Color.black);
     }
 
-    public void rotateshape(){
-        for(int i = 0; i<4; i++){
+    private void setshape() {
+        for (int i = 0; i < 4; i++) {
+            sblocky[i] = allshapesy[nowshape][rotation][i];
+            sblockx[i] = allshapesx[nowshape][rotation][i];
+        }
+    }
 
+    public void rotateshape() {
+        System.out.println(rotation);
+        clearpreviousshape();
+        for (int i = 0; i < 4; i++) {
 
+            sblockx[i] = allshapesx[nowshape][rotation][i];
+            sblocky[i] = allshapesy[nowshape][rotation][i];
 
-            sblockx[i]=allshapesx[nowshape][rotation][i];
-            sblocky[i]=allshapesy[nowshape][rotation][i];
         }
     }
 
@@ -92,14 +102,13 @@ public class gamepanel extends JPanel {
                 e.printStackTrace();
             }
             System.out.println("running");
-            if(move){
+            if (move) {
                 moveshape();
             }
 
-            if(move==false){
+            if (move == false) {
                 createblock();
             }
-
 
             setcolorsarray();
 
@@ -109,58 +118,60 @@ public class gamepanel extends JPanel {
     }
 
     private void createblock() {
-        for(int i =0 ; i<4; i++){
-            if(sblocky[i]==allshapesy[nowshape][rotation][i] && allcolor[sblocky[i]][sblockx[i]]!=Color.black){
-                lose=true;
-            }else{
-                lose=false;
+        for (int i = 0; i < 4; i++) {
+            if (sblocky[i] == allshapesy[nowshape][rotation][i] && allcolor[sblocky[i]][sblockx[i]] != Color.black) {
+                lose = true;
+            } else {
+                lose = false;
             }
         }
-        if(lose){
+        if (lose) {
             System.exit(0);
         }
 
-        owncolor = new Color(r.nextInt(0,255),r.nextInt(0,255),r.nextInt(0,255));
-        nowshape=r.nextInt(0,7);
-        for(int i = 0; i<4; i++){
-            sblockx[i]=allshapesx[nowshape][rotation][i];
-            sblocky[i]=allshapesy[nowshape][rotation][i];
+        owncolor = new Color(r.nextInt(0, 255), r.nextInt(0, 255), r.nextInt(0, 255));
+        nowshape = r.nextInt(0, 7);
+        for (int i = 0; i < 4; i++) {
+            sblockx[i] = allshapesx[nowshape][rotation][i];
+            sblocky[i] = allshapesy[nowshape][rotation][i];
         }
-        move=true;
+        move = true;
+        repaint();
     }
 
     private void moveshape() {
-        
-        for (int i = 0; i < 4; i++) {
-            allcolor[sblocky[i]][sblockx[i]] = Color.black;
-        }
-        
+
+        clearpreviousshape();
+
         for (int i = 0; i < 4; i++) {
             sblocky[i] += 1;
         }
     }
 
-    private void setcolorsarray() {
-        
-        // for (int i : sblocky) {
-        //     if(i==14){
-        //         move=false;
-        //     }
-        // }
+    private void clearpreviousshape() {
+
         for (int i = 0; i < 4; i++) {
-            
+            allcolor[sblocky[i]][sblockx[i]] = Color.black;
+        }
+    }
+
+    private void setcolorsarray() {
+
+        for (int i = 0; i < 4; i++) {
+
             allcolor[sblocky[i]][sblockx[i]] = owncolor;
-    
+
         }
 
-        for (int i = 0; i < 4; i++) {    
-            if(sblocky[i]==14 || allcolor[sblocky[i]+1][sblockx[i]]!=(Color.black) && allcolor[sblocky[i]+1][sblockx[i]]!=owncolor){
-                move=false;
-                lose=false;
+        for (int i = 0; i < 4; i++) {
+            if (sblocky[i] == 14 || allcolor[sblocky[i] + 1][sblockx[i]] != (Color.black)
+                    && allcolor[sblocky[i] + 1][sblockx[i]] != owncolor) {
+                move = false;
+                lose = false;
             }
             // else{
-            //     move=true;
-            // } 
+            // move=true;
+            // }
         }
 
         // for(int i=0;i<=2;i++){
--- src/tetris.java
+++ src/tetris.java
@@ -5,13 +5,13 @@ import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class tetris implements KeyListener{
+public class tetris implements KeyListener {
     private JFrame f;
     private gamepanel game;
-    public tetris(){
+
+    public tetris() {
         f = new JFrame("Tetris");
 
-        
         f.setLayout(null);
         f.setSize(600, 639);
         f.setLocationRelativeTo(null);
@@ -20,37 +20,42 @@ public class tetris implements KeyListener{
         f.addKeyListener(this);
         setpanels();
     }
+
     private void setpanels() {
-        game= new gamepanel();
+        game = new gamepanel();
 
         f.add(game);
         game.gameloop();
     }
+
     public static void main(String[] args) {
         new tetris();
     }
+
     @Override
     public void keyTyped(KeyEvent e) {
         // TODO Auto-generated method stub
     }
+
     @Override
     public void keyPressed(KeyEvent e) {
         // TODO Auto-generated method stub
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
-            System.out.println("key pressed");
-            if(game.rotation==3){
-                game.rotation=0;
-            }else{
-                game.rotation+=1;
-            }
+                System.out.println("key pressed");
+                if (game.rotation == 3) {
+                    game.rotation = 0;
+                } else {
+                    game.rotation += 1;
+                }
                 game.rotateshape();
                 break;
-        
+
             default:
                 break;
         }
     }
+
     @Override
     public void keyReleased(KeyEvent e) {
         // TODO Auto-generated method stub
